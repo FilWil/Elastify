c24715c42a792f46d0b24888bffa321d84276be6
Wilczura <filip.wilczura@capgemini.com>
2018-08-10 08:34:53 +0200

    Initial commit

---
+++ Elastify.ps1
@@ -0,0 +1,18 @@
+﻿Clear
+
+#Starting Kibana Server
+Set-Location -Path C:\ELK_Stack\Kibana\bin
+Start-Process kibana.bat
+
+#Starting Logstash
+Set-Location -Path C:\ELK_Stack\Logstash\bin
+Start-Process logstash.bat
+
+#Starting Filebeat
+Start-Service filebeat
+
+#Starting Metricbeat
+Start-Service metricbeat
+
+#Starting Winlog
+Start-Service winlogbeat
